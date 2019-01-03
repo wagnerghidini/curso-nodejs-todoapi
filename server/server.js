54efa77587a91b6ff37ffbe4b4ca7252d679d98f
Wagner Simões Ghidini <wagnerghidini@gmail.com>
2019-01-03 15:43:06 -0300

    Adicionada rota GET /todos

--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
 
 var app = express();
 app.use(bodyParser.json())
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        results = {todos, statusCode: res.statusCode};
+        res.send(results);
+    }, (err) => {
+        res.status(400).send(`Erro: ${e}`);
+    });
+});
 
 app.post('/todos', (req, res) => {
     todo = new Todo({
@@ -18,10 +27,10 @@ app.post('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(`Erro: ${e}`);
     })
-})
+});
 
 app.listen(3000, () => {
     console.log('Server aberto na porta 3000');
-})
+});
 
-module.exports = {app};
+module.exports = { app };
