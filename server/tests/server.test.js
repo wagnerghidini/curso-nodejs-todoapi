412f9e33c6df0b25bceb473ecf7d7b0efc570289
Wagner Simões Ghidini <wagnerghidini@gmail.com>
2019-01-03 16:32:20 -0300

    Adiciono teste para GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -6,9 +6,16 @@ const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
 const { User } = require('./../models/user');
 
+var todos = [
+    { text: "Primeira Tarefa" },
+    { text: "Segunda Tarefa" },
+]
+
 beforeEach((done) => {
-    Todo.deleteMany({}).then(() => done());
-})
+    Todo.deleteMany({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
+});
 
 describe('POST /todos', () => {
     it('deve criar uma nova tarefa', (done) => {
@@ -20,7 +27,7 @@ describe('POST /todos', () => {
             .expect((res) => expect(res.body.text).toBe('Teste de tarefa'))
             .end((err, res) => {
                 if (err) return done(err);
-                Todo.find().then((todos) => {
+                Todo.find({ text }).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done()
@@ -32,14 +39,26 @@ describe('POST /todos', () => {
         var text = ""
         request(app)
             .post('/todos')
-            .send({text})
+            .send({ text })
             .expect(400)
             .end((err, res) => {
                 if (err) return done(err);
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((err) => done(err));
             });
-        });
+    });
+});
+
+describe('GET /todos', () => {
+    it('deve obter todas as tarefas', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);
+    });
 });
